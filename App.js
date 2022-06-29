4a14fad0c8c2c39b878fa1311cebf582c78dca4f
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-29 19:03:49 +0200

    font set up done

--- App.js
+++ App.js
@@ -3,8 +3,24 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import {
+  useFonts as useRoboto,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [robotoLoaded] = useRoboto({
+    Roboto_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!robotoLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
