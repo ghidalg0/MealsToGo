d90b7106cdc90c8e1d70e07496a12668d381eec2
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-29 13:48:22 +0200

    Safearea view test

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1}}>
+        <View style={{ padding: 16, backgroundColor: '#8ECAE5'}}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: '#FC8500'}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto'/>
+    </>
   );
 }
 
