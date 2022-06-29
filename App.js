02d58d325dd84a1d37b6b9fb57f7d0b8988d97b5
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-29 14:35:10 +0200

    added lint and prettier + rewrote the code

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1}}>
-        <View style={{ padding: 16, backgroundColor: '#8ECAE5'}}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: '#FC8500'}}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <StatusBar style='auto'/>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "#8ECAE5",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#FC8500",
+  },
 });
