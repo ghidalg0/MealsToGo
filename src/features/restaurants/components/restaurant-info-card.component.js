7d6e04e3447d50b2205c7e72a791de8c02a1d2f0
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-29 15:55:23 +0200

    formatting restau cards

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-export const RestaurantInfo = ({ restaurant = {} }) => {
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
     icon,
@@ -18,16 +18,20 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   return (
     <Card elevation={5} style={styles.card}>
       <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text>{name}</Text>
+      <Text style={styles.title}>{name}</Text>
     </Card>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-
+    backgroundColor: "white",
   },
   cover: {
-
+    padding: 16,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
   },
 });
