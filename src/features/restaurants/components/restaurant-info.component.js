48516d77bfb7d595d25fbb84fec5269b94457b7d
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-29 15:41:23 +0200

    created first restaurant card

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text>{name}</Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+
+  },
+  cover: {
+
+  },
+});
