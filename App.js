841bae0047c0a5b3799fecba054a0c377a4677de
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-30 15:37:42 +0200

    navigation tab done

--- App.js
+++ App.js
@@ -9,6 +9,39 @@ import {
 } from "@expo-google-fonts/roboto";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text } from "react-native";
+import { SafeArea } from "./src/utils/safe-area.component";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant-outline",
+  Map: "md-map-outline",
+  Settings: "md-settings-outline",
+};
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
   const [robotoLoaded] = useRoboto({
@@ -23,13 +56,17 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <>
-        <ThemeProvider theme={theme}>
-          <RestaurantsScreen />
-        </ThemeProvider>
-        <ExpoStatusBar style="auto" />
-      </>
-    </NavigationContainer>
+    <>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
