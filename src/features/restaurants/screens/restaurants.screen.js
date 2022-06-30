f17e12e1133c135fafa1bf84cb5203d0fa547f3c
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-30 13:17:19 +0200

    restaurant list done -> now navigation

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -23,15 +23,21 @@ export const RestaurantsScreen = () => (
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <FlatList
-      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+      ]}
       renderItem={() => (
         <Spacer position="bottom" size="medium">
           <RestaurantInfoCard />
         </Spacer>
       )}
       keyExtractor={(item) => item.name}
-      contentContainerStyle={{ padding: 16 }}
     />
   </SafeArea>
 );
