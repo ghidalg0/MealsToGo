fffc3c2eb1c46a446ebef92b87dd84a3492eaef7
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-01 14:17:33 +0200

    passed restaurand card info to the modal and displayed it on the modal

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -37,7 +37,11 @@ export const RestaurantsScreen = ({ navigation }) => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
-              onPress={() => navigation.navigate("RestaurantDetail")}
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", {
+                  restaurant: item,
+                })
+              }
             >
               <Spacer position="bottom" size="medium">
                 <RestaurantInfoCard restaurant={item} />
