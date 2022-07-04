de0b229d2a7b08768bd3ca4359c2248b462325ab
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-04 17:33:50 +0200

    configuration of Firebase

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { initializeApp } from "firebase/app";
 
 import { Navigation } from "./src/infrastructure/navigation/index";
 
@@ -14,6 +15,17 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2SivrHSSuy-7MFNNbk35Mhyv3sqZH7fM",
+  authDomain: "metogo-e9c2c.firebaseapp.com",
+  projectId: "metogo-e9c2c",
+  storageBucket: "metogo-e9c2c.appspot.com",
+  messagingSenderId: "1065520348469",
+  appId: "1:1065520348469:web:e41652bf931a135115607c",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [robotoLoaded] = useRoboto({
