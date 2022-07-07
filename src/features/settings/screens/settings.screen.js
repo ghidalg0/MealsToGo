5449de25283f2bad1258fc259e61cde610b52d18
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-07 12:09:17 +0200

    added avatar placeholder + user identifier on the settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,23 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-import { List } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { List, Avatar } from "react-native-paper";
 import { SafeArea } from "../../../utils/safe-area.component";
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled.View`
+  margin-top: ${(props) => props.theme.space[5]};
+  align-items: center;
+`;
 
 export const SettingsScreen = ({ navigation }) => {
-  const { onLogout } = useContext(AuthenticationContext);
+  const { onLogout, user } = useContext(AuthenticationContext);
   return (
     <SafeArea>
+      <AvatarContainer>
+        <Avatar.Icon size={180} icon="human" backgroundColor="#D00101" />
+        <Spacer position="top" size="large">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
+
       <List.Section>
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
           onPress={() => navigation.navigate("Favourites")}
         />
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
           onPress={onLogout}
