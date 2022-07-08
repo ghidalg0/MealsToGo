46f23a6dabc33e7f6bfc93a5fd5901e9b20252ad
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-08 15:47:12 +0200

    went from retrieving data locally from simulating an api call to firebase google api

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,14 +1,10 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `http://localhost:5001/metogo-e9c2c/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,22 +1,15 @@
-import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
 
 export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
+  return fetch(
+    `http://localhost:5001/metogo-e9c2c/us-central1/placesNearby?location=${location}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
     return {
       ...restaurant,
       address: restaurant.vicinity,
