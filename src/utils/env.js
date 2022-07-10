ac8be66f40def83cb2b5bcaffb47c9cd1b99ec7e
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-10 10:34:54 +0200

    optimozed for android

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,8 @@
+import { Platform } from "react-native";
+
 const liveHost = "https://us-central1-metogo-e9c2c.cloudfunctions.net";
 const localHost = "http://localhost:5001/metogo-e9c2c/us-central1";
+export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const host =
-  process.env.NODE_ENV === "development" ? localHost : liveHost;
+export const isMock = false;
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
